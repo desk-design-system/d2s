7e3c387ce6e63cdd902732b1b07b24d7fdddd2db
kamil-dksa <hayyat552@gmail.com>
2023-01-18 12:47:44 +0500

    -storybook added -.md file updated

--- README.md
+++ README.md
@@ -2,18 +2,27 @@ A progressive [Vue Js](https://vuejs.org/) framework for building efficient and 
 
 
  ![](https://img.shields.io/github/release/pandao/editor.md.svg) ![](https://img.shields.io/bower/v/editor.md.svg)
+#### Description
+----
+D2s is a library for building efficient, scalable [Vue.js](https://vuejs.org/) client-side applications. Under the hood, D2s makes use of [Tailwindcss.](https://tailwindcss.com/), but also, provides compatibility with a wide range of other libraries, like e.g. [Storybook](https://storybook.js.org/), [Headless Ui](https://headlessui.com/) .
 
-#### Principal Sponsors
+
+#### Principal Sponsor
 ----
 [![](https://uploads-ssl.webflow.com/5e109dcc98f2dc1a5ed58148/61125b555ae4913652623f25_RD-2.0-Logo-blog-p-500.png)](https://www.repairdesk.co/our-story#:~:text=RepairDesk%20allows%20users%20to%20keep,and%20manage%20invoices%20and%20receipts.)
 #### Support
 ----
 - Clone D2S library setup into your local system
+- Before setting up your project you should have Prior knowledge of `Storbook` and `tailwindcss`
 - Open cloned project in VS code
-- Create new Branch and make sure the name of the branch should be same as the name of the component for example(git username-componentName)
-- Open the project in vs code go to src/component/ create the folder with the same name of your required component and add an index.vue file in it 
+- Create new Branch and make sure the name of the branch should be same as the name of the component for example`(git username-componentName)`
+- Open the project in vs code go to `src/component/` create the folder with the name of your required component and add an `index.vue` file in it.
+- After creating your component in `index.vue` create another file in the same directory  with name of your directory with `storesi.js`  example:   `Buttons.stories.js`
+- This will create your story you can run it on your local system. Simply run this command to test your component  `npm run storybook` . 
 - Develope the component with the required props and make sure you should add comments againt every prop so we can easily undertsnad the usage of  prop
 - Create your merge request with comments owner will  test the changes and merge the code later.
+
+
 
 #### Contributers
 ----
---
+++ src/components/buttons/Button.stories.js
@@ -0,0 +1,92 @@
+// import MyButton from './Button.vue';
+import DdButton from "./index.vue"
+import { action } from "@storybook/addon-actions"
+
+// More on default export: https://storybook.js.org/docs/vue/writing-stories/introduction#default-export
+export default {
+  title: 'Example/Button',
+  component: DdButton,
+  // More on argTypes: https://storybook.js.org/docs/vue/api/argtypes
+  argTypes: {
+    color: { 
+      control:{
+        type: 'select' 
+      },
+      options: ['primary','danger', 'success', 'white'],
+      description: "Change color value as per the requirement primary / danger / success / white",
+      table:{
+        defaultValue:{
+          summary: "-"
+        }
+      }
+     },
+     title:{
+      description: "Change title value as per the requirement",
+      table:{
+        defaultValue:{
+          summary: "Button"
+        }
+      }
+     },
+    // onClick: {
+    //   action: "clicked"
+    // },
+    size: {
+      control: { type: 'select' },
+      options: ['mini','small', 'medium', 'large','xLarge',],
+      description: "Accepted value mini / small / medium / large / xLarge ",
+      table:{
+        defaultValue:{
+          summary: "medium"
+        }
+      }
+    },
+    block:{
+      type: 'boolean',
+      description: "To make button width full to parent div chnage boolean value to true",
+      table:{
+        defaultValue:{
+          summary: "False"
+        }
+      }
+    },
+    type: {
+      control: { type: 'select' },
+      options: ['default','round','text', 'circle',],
+       description: "By default button type is color less so change the props default / round / text / circle",
+      table:{
+        defaultValue:{
+          summary: "-"
+        }
+      }
+    },
+  },
+};
+
+// More on component templates: https://storybook.js.org/docs/vue/writing-stories/introduction#using-args
+const Template = (args) => ({
+  // Components used in your story `template` are defined in the `components` object
+  components: { DdButton },
+  // The story's `args` need to be mapped into the template through the `setup()` method
+  setup() {
+    const onClickMethod =  (() => action("clicked"))
+    return { args, onClickMethod };
+
+  },
+  // And then the `args` are bound to your component with `v-bind="args"`
+  template: '<dd-button @click="onClickMethod" v-bind="args" />  ',
+});
+
+
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/vue/writing-stories/args
+console.log(Primary.args, "Primary")
+Primary.args = {
+  color: 'primary',
+  title: 'Button',
+  size: 'small',
+  type: 'default'
+};
+
+
