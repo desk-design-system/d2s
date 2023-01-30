8a649f2b78e03d05314b29ba3c46744731260e2d
kamil-dksa <hayyat552@gmail.com>
2023-01-30 12:45:31 +0500

    -typography -typography story

---
+++ src/components/typography/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p  v-bind="$attrs" >{{ title }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: "The face of the moon in shadow.",
+    },
+    // className: {
+    //   type: String,
+    //   default: "",
+    // },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
