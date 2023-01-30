93295d7405df56fabc0eba9069f4245d44ab5300
kamil-dksa <hayyat552@gmail.com>
2023-01-30 12:47:46 +0500

    .

--- src/components/typography/Typography.stories.js
+++ src/components/typography/Typography.stories.js
@@ -16,39 +16,39 @@ export default {
                 }
             }
         },
-        // class: {
-        //     control: {
-        //         type: 'select'
-        //     },
-        //     options: ["dd-text-xs",
-        //         "dd-text-sm",
-        //         "dd-text-base",
-        //         "dd-text-lg",
-        //         "dd-text-xl",
-        //         "dd-text-2xl",
-        //         "dd-text-3xl",
-        //         "dd-text-4xl",
-        //         "dd-text-5xl",
-        //         "dd-text-6xl",
-        //         "dd-text-7xl",
-        //         "dd-text-8xl",
-        //         "dd-text-9xl",
-        //         "dd-font-thin",
-        //         "dd-font-extralight",
-        //         "dd-font-light",
-        //         "dd-font-normal",
-        //         "dd-font-medium",
-        //         "dd-font-semibold",
-        //         "dd-font-bold",
-        //         "dd-font-extrabold",
-        //         "dd-font-black",],
-        //     description: "Change color value as per the requirement primary / danger / success / white",
-        //     table: {
-        //         defaultValue: {
-        //             summary: "-"
-        //         }
-        //     }
-        // },
+        class: {
+            control: {
+                type: 'select'
+            },
+            options: ["dd-text-xs",
+                "dd-text-sm",
+                "dd-text-base",
+                "dd-text-lg",
+                "dd-text-xl",
+                "dd-text-2xl",
+                "dd-text-3xl",
+                "dd-text-4xl",
+                "dd-text-5xl",
+                "dd-text-6xl",
+                "dd-text-7xl",
+                "dd-text-8xl",
+                "dd-text-9xl",
+                "dd-font-thin",
+                "dd-font-extralight",
+                "dd-font-light",
+                "dd-font-normal",
+                "dd-font-medium",
+                "dd-font-semibold",
+                "dd-font-bold",
+                "dd-font-extrabold",
+                "dd-font-black",],
+            description: "Change color value as per the requirement primary / danger / success / white",
+            table: {
+                defaultValue: {
+                    summary: "-"
+                }
+            }
+        },
     },
 };
 
