4b051bbe888e92160cdfeccc081eebce0af737fe
kamil-dksa <hayyat552@gmail.com>
2023-02-02 12:21:50 +0500

    custome colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+       primary: '#d12026',
+       secondary: '#ecc94b',
+    },
+    },
+     
   },
   variants: {
     extend: {},
