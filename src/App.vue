1532c9894547886ad914a0f7ac93caf89b5ed4b4
Tasnim Afsar Khan <tasnimafsarkhan321@gmail.com>
2023-02-03 16:50:48 +0500

    Add avatar elements

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,21 @@
     <div class="flex justify-between">
       <p class="dd-text-primary">Hello</p>
       <dd-button color="success"> kamil </dd-button>
+      <div class="dd-flex">
+        <span v-for="temp in sizes">
+          <img-avatar type="default" :size="temp" srcLink="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"></img-avatar>
+        </span>
+      </div>
+      <div class="dd-flex">
       <span v-for="temp in sizes">
-        <img-avatar :size="temp" srcLink="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"></img-avatar>
+        <img-avatar type="group" :size="temp" srcLink="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"></img-avatar>
       </span>
+      </div>
+      <div class="dd-flex">
+      <span v-for="temp in sizes">
+        <img-avatar type="text" :size="temp" title="Button"></img-avatar>
+      </span>
+    </div>
       <img src="./assets/test.svg" alt="">
       <img src="./assets/Add User.svg" alt="">
       <svgIcon2 />
--- src/components/avatars/index.vue
+++ src/components/avatars/index.vue
@@ -22,17 +22,39 @@
         <img
             v-bind="$attrs"
             :class="{
-                'dd-h-6 dd-w-6': size === 'default',
+                'dd-h-6 dd-w-6': size === 'mini',
             }" 
-            class="dd-inline-flex dd-inline-block dd-h-6 dd-w-6 dd-rounded-full dd-ring-2 dd-ring-white" 
+            class="dd-inline-flex dd-inline-block dd-h-6 dd-w-6 dd-space-x-1 dd-overflow-hidden dd-rounded-full dd-ring-2 dd-ring-white" 
             :src="srcLink">
       </div>
+      
+      <div
+      v-else-if="type == 'text'">
+      <span
+        v-bind="$attrs"
+            :class="{
+            'dd-text-white':color == '',
+            'dd-h-6 dd-w-6 dd-leading-none dd-text-xs dd-font-medium': size === 'mini',
+            'dd-h-8 dd-w-8 dd-leading-none dd-text-sm dd-font-medium': size === 'small',
+            'dd-h-10 dd-w-10 dd-leading-none dd-font-medium': size === 'medium',
+            'dd-h-12 dd-w-12 dd-leading-none dd-text-lg dd-font-medium': size === 'large',
+            'dd-h-14 dd-w-14 dd-leading-none dd-text-xl dd-font-medium': size === 'xLarge',
+            }"
+            class="dd-inline-flex dd-items-center dd-font-medium dd-justify-center dd-rounded-full dd-bg-gray-500"
+            ><slot>{{ title.charAt(0) }}</slot></span>
+        </div>
     </div>
   </template>
   
   <script>
   export default {
     props: {
+      title: {
+        type: String,
+        validator: function (value) {
+        },
+        default: "",
+      },
       srcLink: {
         type: String,
         validator: function (value) {
