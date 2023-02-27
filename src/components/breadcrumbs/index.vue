05a3d8d6002d5d0e8ca7514945a2fa40c3368e6f
kamilRiaz <hayyat552@gmail.com>
2023-02-27 13:11:33 +0500

    basic structure

---
+++ src/components/breadcrumbs/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav class="flex" aria-label="Breadcrumb">
+    <ol role="list" class="flex space-x-4 rounded-md bg-white px-6 shadow">
+      <li class="flex">
+        <div class="flex items-center">
+          <a href="#" class="text-gray-400 hover:text-gray-500">
+            <HomeIcon class="h-5 w-5 flex-shrink-0" aria-hidden="true" />
+            <span class="sr-only">Home</span>
+          </a>
+        </div>
+      </li>
+      <li v-for="page in pages" :key="page.name" class="flex">
+        <div class="flex items-center">
+          <svg class="h-full w-6 flex-shrink-0 text-gray-200" viewBox="0 0 24 44" preserveAspectRatio="none" fill="currentColor" aria-hidden="true">
+            <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
+          </svg>
+          <a :href="page.href" class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700" :aria-current="page.current ? 'page' : undefined">{{ page.name }}</a>
+        </div>
+      </li>
+    </ol>
+  </nav>
+</template>
+
+<script setup>
+import { HomeIcon } from '@heroicons/vue/20/solid'
+
+const pages = [
+  { name: 'Projects', href: '#', current: false },
+  { name: 'Project Nero', href: '#', current: true },
+]
+</script>
