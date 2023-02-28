7a429b326f4b467933d6c877736cd144ea3079c0
devfarhad <farhad03009776700@gmail.com>
2023-02-28 14:37:32 +0500

    left bar icon

---
+++ src/components/icons/layoutDashboardIcon.vue
@@ -0,0 +1,13 @@
+<template>
+    <svg width="28px" height="28px" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <g id="Symbols" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+            <g id="Icons/20px/Dashboard">
+                <polygon id="Path" points="0 0 20 0 20 20 0 20"></polygon>
+                <rect id="Rectangle" stroke="#1F2937" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" x="3.33333333" y="3.33333333" width="5" height="5" rx="0.833333333"></rect>
+                <rect id="Rectangle" stroke="#1F2937" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" x="11.6666667" y="3.33333333" width="5" height="5" rx="0.833333333"></rect>
+                <rect id="Rectangle" stroke="#1F2937" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" x="3.33333333" y="11.6666667" width="5" height="5" rx="0.833333333"></rect>
+                <rect id="Rectangle" stroke="#1F2937" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" x="11.6666667" y="11.6666667" width="5" height="5" rx="0.833333333"></rect>
+            </g>
+        </g>
+    </svg>
+</template>
