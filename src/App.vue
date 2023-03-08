300d5446e366d94b26ae96d1bbbaa1bf6096b2e2
kamilRiaz <hayyat552@gmail.com>
2023-03-08 20:30:46 +0500

    notification as  plugin

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="dd-container dd-mx-auto dd-m-5">
+    <button @click="showSuccess"> show Success </button>
   </div>
 </template>
-<script>
-
+<script >
 export default {
   data() {
     return {
@@ -11,7 +11,22 @@ export default {
     };
   },
   components:{
-
+    
   }
 };
+</script>
+
+<script setup>
+import { onMounted } from "vue";
+import {useAlert} from "./components/notification/index.js"
+const alert = useAlert()
+const showSuccess = () => {
+  alert.success("kamil", "helooo", 3000, 'topRight')
+  // alert.warning("kamil", "helooo")
+  // alert.info("kamil", "helooo")
+  // alert.error("kamil", "helooo")
+}
+onMounted(() =>{
+  console.log(alert)
+})
 </script>
---
+++ src/components/notification/index.vue
@@ -0,0 +1,36 @@
+<script>
+import RDAlertMessage from "./notificationUI.vue";
+import { render } from "vue";
+import { removeElement } from "./helpers";
+
+export default {
+  components: {
+    RDAlertMessage,
+  },
+  mounted() {
+    console.log("setting alert")
+    this.showAlert = true;
+  },
+  methods: {
+    dismiss() {
+      setTimeout(() => {
+        render(null, this.$el);
+        removeElement(this.$el);
+      }, 2000);
+    },
+  },
+  data() {
+    return {
+      showAlert: false,
+    };
+  },
+};
+</script>
+
+<template>
+  <RDAlertMessage
+    ref="root"
+    v-model:show="showAlert"
+    @removeElement="dismiss"
+  />
+</template>
