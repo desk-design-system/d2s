441395b834eb0568d41cda8d6780c7d3f2f176d2
kamilRiaz <hayyat552@gmail.com>
2023-03-09 15:04:33 +0500

    warning daner color added

--- src/components/buttons/index.vue
+++ src/components/buttons/index.vue
@@ -1,56 +1,58 @@
 <template>
-  <div>
-    <!-- default -->
-    <button v-bind="$attrs" v-if="type == 'default'" :class="{...defaultButton}" 
-    class="
-        dd-inline-flex
-        dd-items-center
-        dd-shadow-sm
-        dd-font-medium
-        dd-capitalize
-        dd-whitespace-nowrap
-      ">
-      <!-- <component
+  <!-- <div> -->
+  <!-- default -->
+  <button v-bind="$attrs" v-if="type == 'default'" :class="{ ...defaultButton }" class="
+          dd-inline-flex
+          dd-items-center
+          dd-shadow-sm
+          dd-font-medium
+          dd-capitalize
+          dd-whitespace-nowrap
+        ">
+    <!-- <component
         v-if="prependIcon != null"
         :size="iconSize"
         :is="prependIcon + '-icon'"
         :class="iconstyle"
         :style="prependIcon != '' ? 'margin-righ:8px' : ''"
       /> -->
-      <slot>{{ title }}</slot>
-      <!-- <component
+    <slot>{{ title }}</slot>
+    <!-- <component
         v-if="appendIcon != null"
         :style="appendIcon == '' ? '' : 'margin-left:8px'"
         :class="iconstyle"
         :is="appendIcon + '-icon'"
         :size="iconSize"
       /> -->
-    </button>
+  </button>
 
-    <!-- round -->
-    <button v-bind="$attrs" v-else-if="type == 'round'" :class="{
-      'border-gray-300 dd-text-gray-700 dd-border  focus:dd-outline-none':
-        color == '',
-      ' dd-px-2.5 dd-py-1.5 dd-text-xs': size === 'xs',
-      ' dd-px-3 dd-py-2 dd-text-sm': size === 'sm',
-      ' dd-px-4 dd-py-2 dd-text-sm': size === 'base',
-      ' dd-px-4 dd-py-2 dd-text-base': size === 'lg',
-      ' dd-px-6 dd-py-3 dd-text-base': size === 'xl',
-      'dd-bg-teal-600 dd-text-white hover:dd-bg-teal-700':
-        color === 'primary',
-      'dd-bg-red-600 dd-text-white hover:dd-bg-red-700': color === 'danger',
-      'dd-cursor-not-allowed !dd-bg-gray-200 !dd-text-gray-400 hover:dd-bg-gray-300': disable,
-    }" class="
-        dd-inline-flex
-        dd-items-center
-        dd-shadow-sm
-        dd-rounded-full
-        dd-font-medium
-        dd-capitalize
-        dd-whitespace-nowrap
-      ">
-      <slot>{{ title }}</slot>
-      <!-- <component
+  <!-- round -->
+  <button v-bind="$attrs" v-else-if="type == 'round'" :class="{
+    'border-gray-300 dd-text-gray-700 dd-border  focus:dd-outline-none':
+      color == '',
+    ' dd-px-2.5 dd-py-1.5 dd-text-xs': size === 'xs',
+    ' dd-px-3 dd-py-2 dd-text-sm': size === 'sm',
+    ' dd-px-4 dd-py-2 dd-text-sm': size === 'base',
+    ' dd-px-4 dd-py-2 dd-text-base': size === 'lg',
+    ' dd-px-6 dd-py-3 dd-text-base': size === 'xl',
+    'dd-bg-teal-600 dd-text-white hover:dd-bg-teal-700':
+      color === 'primary',
+    'dd-bg-yellow-600 dd-text-white hover:dd-bg-yellow-700': color === 'warning',
+    'dd-bg-green-600 dd-text-white hover:dd-bg-green-700':
+      color === 'success',
+    'dd-bg-red-600 dd-text-white hover:dd-bg-red-700': color === 'danger',
+    'dd-cursor-not-allowed !dd-bg-gray-200 !dd-text-gray-400 hover:dd-bg-gray-300': disable,
+  }" class="
+          dd-inline-flex
+          dd-items-center
+          dd-shadow-sm
+          dd-rounded-full
+          dd-font-medium
+          dd-capitalize
+          dd-whitespace-nowrap
+        ">
+    <slot>{{ title }}</slot>
+    <!-- <component
         v-if="prependIcon != null"
         :size="iconSize"
         :is="prependIcon + '-icon'"
@@ -65,26 +67,29 @@
         :is="appendIcon + '-icon'"
         :size="iconSize"
       /> -->
-    </button>
-    <button v-bind="$attrs" v-else-if="type == 'text'" :class="{
-      ' dd-text-gray-700  hover:dd-text-gray-900 ': color == '',
-      ' dd-px-2.5 dd-py-1.5 dd-text-xs': size === 'xs',
-      ' dd-px-3 dd-py-2 dd-text-sm': size === 'sm',
-      ' dd-px-4 dd-py-2 dd-text-sm': size === 'base',
-      ' dd-px-4 dd-py-2 dd-text-base': size === 'lg',
-      ' dd-px-6 dd-py-3 dd-text-base': size === 'xl',
-      ' dd-text-teal-700 hover:dd-text-teal-700': color === 'primary',
-      ' dd-text-red-600 hover:dd-text-red-700': color === 'danger',
-      'dd-cursor-not-allowed  !dd-text-gray-400 hover:dd-text-gray-500 hover:dd-text-transparent	': disable,
-    }" class="
-        dd-inline-flex
-        dd-items-center
-        dd-font-medium
-        dd-capitalize
-        dd-whitespace-nowrap
-      ">
-      <slot>{{ title }}</slot>
-      <!-- <component
+  </button>
+  <button v-bind="$attrs" v-else-if="type == 'text'" :class="{
+    ' dd-text-gray-700  hover:dd-text-gray-900 ': color == '',
+    ' dd-px-2.5 dd-py-1.5 dd-text-xs': size === 'xs',
+    ' dd-px-3 dd-py-2 dd-text-sm': size === 'sm',
+    ' dd-px-4 dd-py-2 dd-text-sm': size === 'base',
+    ' dd-px-4 dd-py-2 dd-text-base': size === 'lg',
+    ' dd-px-6 dd-py-3 dd-text-base': size === 'xl',
+    ' dd-text-teal-700 hover:dd-text-teal-700': color === 'primary',
+    ' dd-text-red-600 hover:dd-text-red-700': color === 'danger',
+    'dd-text-yellow-600  hover:dd-text-yellow-700': color === 'warning',
+    'dd-text-green-600  hover:dd-text-green-700':
+      color === 'success',
+    'dd-cursor-not-allowed  !dd-text-gray-400 hover:dd-text-gray-500 hover:dd-text-transparent	': disable,
+  }" class="
+          dd-inline-flex
+          dd-items-center
+          dd-font-medium
+          dd-capitalize
+          dd-whitespace-nowrap
+        ">
+    <slot>{{ title }}</slot>
+    <!-- <component
         v-if="prependIcon != null"
         :size="iconSize"
         :is="prependIcon + '-icon'"
@@ -99,31 +104,31 @@
         :is="appendIcon + '-icon'"
         :size="iconSize"
       /> -->
-    </button>
+  </button>
 
-    <button v-else-if="type == 'circle'" :class="{
-      'dd-border-gray-300 dd-text-gray-700 dd-border hover:dd-bg-gray-50 focus:dd-outline-none':
-        color == '',
-      ' dd-p-1 dd-text-xs': size === 'xs',
-      ' dd-p-1.5 dd-text-sm ': size === 'sm',
-      ' dd-p-2 dd-text-sm ': size === 'base',
-      ' dd-p-2  dd-text-base': size === 'lg',
-      ' dd-p-3 dd-text-base': size === 'xl',
-      'dd-bg-teal-600 dd-text-white hover:dd-bg-teal-700':
-        color === 'primary',
-      'dd-bg-red-600 dd-text-white hover:dd-bg-red-700': color === 'danger',
-      'dd-cursor-not-allowed !dd-bg-gray-200 !dd-text-gray-400 hover:dd-bg-gray-300': disable,
-    }" class="
-        dd-inline-flex
-        dd-items-center
-        dd-shadow-sm
-        dd-font-medium
-        dd-rounded-full
-        dd-capitalize
-        dd-whitespace-nowrap
-      ">
-      <slot>{{ title }}</slot>
-      <!-- <component
+  <button v-else-if="type == 'circle'" :class="{
+    'dd-border-gray-300 dd-text-gray-700 dd-border hover:dd-bg-gray-50 focus:dd-outline-none':
+      color == '',
+    ' dd-p-1 dd-text-xs': size === 'xs',
+    ' dd-p-1.5 dd-text-sm ': size === 'sm',
+    ' dd-p-2 dd-text-sm ': size === 'base',
+    ' dd-p-2  dd-text-base': size === 'lg',
+    ' dd-p-3 dd-text-base': size === 'xl',
+    'dd-bg-teal-600 dd-text-white hover:dd-bg-teal-700':
+      color === 'primary',
+    'dd-bg-red-600 dd-text-white hover:dd-bg-red-700': color === 'danger',
+    'dd-cursor-not-allowed !dd-bg-gray-200 !dd-text-gray-400 hover:dd-bg-gray-300': disable,
+  }" class="
+          dd-inline-flex
+          dd-items-center
+          dd-shadow-sm
+          dd-font-medium
+          dd-rounded-full
+          dd-capitalize
+          dd-whitespace-nowrap
+        ">
+    <slot>{{ title }}</slot>
+    <!-- <component
         v-if="prependIcon != null"
         :size="iconSize"
         :is="prependIcon + '-icon'"
@@ -138,8 +143,8 @@
         :is="appendIcon + '-icon'"
         :size="iconSize"
       /> -->
-    </button>
-  </div>
+  </button>
+  <!-- </div> -->
 </template>
 
 <script>
@@ -217,6 +222,10 @@ export default {
           this.disable,
         "dd-bg-teal-600 dd-text-white hover:dd-bg-teal-700":
           this.color === "primary",
+        "dd-bg-yellow-600 dd-text-white hover:dd-bg-yellow-700":
+          this.color === "warning",
+        "dd-bg-green-600 dd-text-white hover:dd-bg-green-700":
+          this.color === "success",
         "dd-bg-red-600 dd-text-white hover:dd-bg-red-700":
           this.color === "danger",
         // "dd-bg-green-600 dd-text-white hover:dd-bg-green-700":
@@ -235,6 +244,4 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
