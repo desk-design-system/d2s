f021dc6b79d96215ddb1819504a30582e87fc66e
kamilRiaz <kamilriaz@blueorbits.com>
2023-03-13 14:24:08 +0500

    icons added

---
+++ src/components/icons/Edit.vue
@@ -0,0 +1,14 @@
+<template>
+<svg width="12px" height="12px" viewBox="0 0 12 12" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <title>Icons/16px/Plus Copy</title>
+    <g id="⚡️-First-Release-Multistore" stroke="currentColor" stroke-width="1" fill="none" fill-rule="evenodd">
+        <g id="New-Unit-Flow-1" transform="translate(-1349.000000, -329.000000)">
+            <g id="Icons/16px/Plus-Copy" transform="translate(1347.222875, 326.694432)">
+                <polygon id="Path" points="0 0 16 0 16 16 0 16"></polygon>
+                <path d="M2.66666667,13.3333333 L5.33333333,13.3333333 L12.3333333,6.33333333 C13.069713,5.59695366 13.069713,4.40304635 12.3333333,3.66666669 C11.5969537,2.93028703 10.4030463,2.93028702 9.66666667,3.66666667 L2.66666667,10.6666667 L2.66666667,13.3333333" id="Path" stroke="currentColor" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"></path>
+                <line x1="9" y1="4.33333333" x2="11.6666667" y2="7" id="Path" stroke="currentColor" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"></line>
+            </g>
+        </g>
+    </g>
+</svg>
+</template>
