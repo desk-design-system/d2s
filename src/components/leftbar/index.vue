aad3fc1766533e4a2d2e5c9ca21db083708f6cdc
kamilRiaz <hayyat552@gmail.com>
2023-03-13 16:45:25 +0500

    z-index added

--- src/components/leftbar/index.vue
+++ src/components/leftbar/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="dd-fixed dd-top-14">
+  <div class="dd-fixed dd-top-14 dd-z-10">
       <aside ref="leftBarRef" id="default-sidebar" class="dd-shadow dd-fixed dd-top-15 dd-left-0 dd-z-40 dd-w-16 dd-h-screen dd-transition-transform dd-translate-x-full sm:dd-translate-x-0" aria-label="Sidebar">
         <div class="dd-h-full dd-py-4 dd-overflow-y-auto dd-bg-white">
             <ul class="space-y-2">
