466c764b5c9dcf46a893b9118aef2600d82ba73d
kamilRiaz <kamilriaz@blueorbits.com>
2023-03-24 12:00:59 +0500

    validation added

--- src/components/input/index.vue
+++ src/components/input/index.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
-    <label v-if="label" class="dd-block dd-text-sm dd-font-medium dd-text-gray-700 dd-mb-1">{{ label }}</label>
+    <label v-if="label" class="dd-block dd-text-sm dd-font-medium dd-text-gray-700 dd-mb-1">{{ label }}  <span v-if="isRequired" class="dd-text-red-500 ">*</span> </label>
     <div class="dd-relative  dd-rounded-md ">
       <div v-if="$slots.prefix"
         class="!dd-pointer-events-none !dd-absolute !dd-inset-y-0 !dd-left-0 !dd-flex !dd-items-center !dd-pl-3 !dd-pr-10">
         <slot name="prefix">
         </slot>
       </div>
-      <input :class="[$slots.prefix ? 'dd-pl-10' : 'dd-pl-2' ]" v-bind="$attrs" v-model="inputModelValue" :type="type"
-        class="!dd-block !dd-w-full !dd-rounded-md !dd-border-gray-300  focus:!dd-border-teal-600 dd-focus:!dd-ring-teal-600 sm:!dd-text-sm focus:ring-2 focus:dd-ring-inset focus:dd-ring-teal-600 dd-shadow-sm"
+      <input :class="[$slots.prefix ? 'dd-pl-10' : 'dd-pl-2', hasError ?  '!dd-border-red-600' : '!dd-border-gray-300'  ]" v-bind="$attrs" v-model="inputModelValue" :type="type"
+        class="!dd-block !dd-w-full !dd-rounded-md   focus:!dd-border-teal-600 dd-focus:!dd-ring-teal-600 sm:!dd-text-sm focus:ring-2 focus:dd-ring-inset focus:dd-ring-teal-600 dd-shadow-sm"
         :placeholder="placeholder" />
       <div v-if="$slots.suffix"
         class="!dd-pointer-events-none !dd-absolute !dd-inset-y-0 !dd-right-0 !dd-flex !dd-items-center !dd-pl-3 !dd-pr-3">
@@ -16,6 +16,7 @@
         </slot>
       </div>
     </div>
+    <span v-if="errorMessage" class="dd-text-xs dd-text-red-500 dd-capitalize">{{ errorMessage }}</span>
   </div>
 </template>
 
@@ -26,6 +27,14 @@ const props = defineProps( {
   label: {
     type: String,
     default: "",
+  },
+  errorMessage: {
+    type: String,
+    default: "",
+  },
+  isRequired: {
+    type: Boolean,
+    default: false,
   },
   placeholder: {
     type: String,
@@ -47,6 +56,13 @@ const inputModelValue = computed( {
   set ( val ) {
     emits( "update:modelValue", val )
     emits( "change", val )
+  }
+} )
+const hasError = computed( () => {
+  if(props.errorMessage){
+    return true
+  } else{
+  return false
   }
 } )
 </script>
