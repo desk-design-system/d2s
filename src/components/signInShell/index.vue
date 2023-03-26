82a67a39ab552bd9deec02156f3ec0415864c03c
kamilRiaz <hayyat552@gmail.com>
2023-03-26 16:45:40 +0500

    ui issue fixed

--- src/components/signInShell/index.vue
+++ src/components/signInShell/index.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="dd-bg-gray-100 dd-h-screen">
     <div class="dd-flex dd-flex-col dd-justify-center dd-align-center ">
-      <div class="dd-text-center dd-w-[215px] dd-mx-auto dd-mt-20 dd-mb-24">
+      <div class=" dd-w-[215px] dd-mx-auto dd-mt-20 dd-mb-24">
         <img src="../../assets/rdlogo.png" />
       </div>
       <div v-if="showMessage">
-        <ddCard elevation="sm" :x="14" :y="14" class="dd-w-[620px] dd-mx-auto">
+        <ddCard elevation="sm" :x="14" :y="14" className="dd-w-[620px] dd-mx-auto">
           hello
         <slot></slot>
       </ddCard>
       </div>
-      <div v-else>
-        <p class="dd-text-3xl dd-text-gray-700 dd-font-bold dd-mx-auto dd-mb-6">{{title}}</p>
-      <ddCard elevation="sm" :x="10" :y="8" class="dd-w-[450px] dd-mx-auto">
+      <div class="dd-mx-auto" v-else>
+        <p class="dd-text-center dd-text-3xl dd-text-gray-700 dd-font-bold  dd-mb-6">{{title}}</p>
+      <ddCard elevation="sm" :x="10" :y="8" className="dd-w-[450px] ">
         <slot></slot>
       </ddCard>
       </div>
