ec055be0b8b7a0d0a08cd135a8dfd7f6466cbca1
kamilRiaz <hayyat552@gmail.com>
2023-03-27 14:42:59 +0500

    topbar aavtar drop down added

--- src/components/topbar/index.vue
+++ src/components/topbar/index.vue
@@ -3,19 +3,19 @@
     <div class="dd-flex dd-items-center dd-justify-center dd-opacity-100 dd-h-14 dd-w-32 dd-bg-teal-700 dd-rounded-r-full">
       <span class="dd-opacity-100 dd-text-sm dd-font-medium dd-not-italic dd-tracking-normal dd-text-center dd-align-center dd-text-white dd-leading-4">Repair Shop</span>
     </div>
-    <div class="dd-w-full dd-mr-2 dd-block dd-flex-grow lg:dd-flex lg:dd-items-center lg:dd-w-auto">
+    <div class="dd-flex dd-items-center dd-justify-end dd-w-full dd-mr-6 dd-flex-grow lg:dd-flex lg:dd-items-center lg:dd-w-auto">
       <!-- <div class="dd-text-sm lg:dd-flex-grow">
         <ddinput @update:modelValue="value" @change="emitSparkSearchEntry" class="dd-w-3/5 dd-ml-12" />
       </div> -->
-      <dddrop type="avatar" placement="left" avatarSize="small" :avatarLink="props.srcLink" :options="props.options" />
+      <dd-drop type="avatar" placement="right" avatarSize="small" :avatarLink="props.srcLink" :options="props.options" />
     </div>
   </nav>
 </template>
 
 <script setup>
 import { defineComponent, defineProps } from 'vue'
-import ddinput from '../input/index.vue'
-import ddavatar from '../avatars/index.vue'
+// import ddinput from '../input/index.vue'
+import ddDrop from '../dropdown/index.vue'
 import { emit } from 'process';
 
 const props = defineProps( {
