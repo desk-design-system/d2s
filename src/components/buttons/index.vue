d27c78b7a9c6a1e4ea4b5f1722fc374918db6ef0
kamilRiaz <kamilriaz@blueorbits.com>
2023-03-30 13:18:54 +0500

    button loader class icon margin icon sizing

--- src/components/buttons/index.vue
+++ src/components/buttons/index.vue
@@ -2,20 +2,28 @@
   <!-- <div> -->
   <!-- default -->
   <button v-bind="$attrs" v-if="type == 'default'" :class="{ ...defaultButton }" class="
-          dd-inline-flex
-          dd-items-center
-          dd-shadow-sm
-          dd-font-semibold
-          dd-capitalize
-          dd-whitespace-nowrap
-        ">
-      <svgIcon  v-if="prefix" class="dd-mr-[6px]" :icon="icon" :size="iconSize" />
-      <svg v-if="loader" aria-hidden="true" :class="[size == 'xs' || size == 'sm' ? 'dd-h-3 dd-w-3' : 'dd-h-4 dd-w-4']" class=" dd-animate-spin dd-fill-teal-600 dd-text-white-50 " viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
-    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
-  </svg>
-    <slot v-if="!loader"> {{ title }}</slot>
-    <svgIcon class="dd-ml-[6px]" v-if="suffix" :icon="icon" :size="iconSize" />
+              dd-inline-flex
+              dd-items-center
+              dd-shadow-sm
+              dd-font-semibold
+              dd-capitalize
+              dd-whitespace-nowrap
+            ">
+    <svg v-if="loader" aria-hidden="true" :class="[size == 'xs' || size == 'sm' ? 'dd-h-3.5 dd-w-3.5' : 'dd-h-4 dd-w-4']"
+      class=" dd-animate-spin dd-fill-teal-600 dd-text-white-50 " viewBox="0 0 100 101" fill="none"
+      xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+        fill="currentColor" />
+      <path
+        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+        fill="currentFill" />
+    </svg>
+    <svgIcon v-if="prefix && !loader" :icon="icon" :size="btnIconSize" />
+    <span :class="[prefixSpace, sufixSpace]">
+      <slot v-if="!loader"> {{ title }}</slot>
+    </span>
+    <svgIcon v-if="suffix && !loader" :icon="icon" :size="btnIconSize" />
   </button>
 
   <!-- round -->
@@ -35,14 +43,14 @@
     'dd-bg-red-600 dd-text-white hover:dd-bg-red-700': color === 'danger',
     'dd-cursor-not-allowed !dd-bg-gray-200 !dd-text-gray-400 hover:dd-bg-gray-300': disable,
   }" class="
-          dd-inline-flex
-          dd-items-center
-          dd-shadow-sm
-          dd-rounded-full
-          dd-font-medium
-          dd-capitalize
-          dd-whitespace-nowrap
-        ">
+              dd-inline-flex
+              dd-items-center
+              dd-shadow-sm
+              dd-rounded-full
+              dd-font-medium
+              dd-capitalize
+              dd-whitespace-nowrap
+            ">
     <slot>{{ title }}</slot>
   </button>
   <button v-bind="$attrs" v-else-if="type == 'text'" :class="{
@@ -59,12 +67,12 @@
       color === 'success',
     'dd-cursor-not-allowed  !dd-text-gray-400 hover:dd-text-gray-500 hover:dd-text-transparent	': disable,
   }" class="
-          dd-inline-flex
-          dd-items-center
-          dd-font-medium
-          dd-capitalize
-          dd-whitespace-nowrap
-        ">
+              dd-inline-flex
+              dd-items-center
+              dd-font-medium
+              dd-capitalize
+              dd-whitespace-nowrap
+            ">
     <slot>{{ title }}</slot>
   </button>
 
@@ -81,14 +89,14 @@
     'dd-bg-red-600 dd-text-white hover:dd-bg-red-700': color === 'danger',
     'dd-cursor-not-allowed !dd-bg-gray-200 !dd-text-gray-400 hover:dd-bg-gray-300': disable,
   }" class="
-          dd-inline-flex
-          dd-items-center
-          dd-shadow-sm
-          dd-font-medium
-          dd-rounded-full
-          dd-capitalize
-          dd-whitespace-nowrap
-        ">
+              dd-inline-flex
+              dd-items-center
+              dd-shadow-sm
+              dd-font-medium
+              dd-rounded-full
+              dd-capitalize
+              dd-whitespace-nowrap
+            ">
     <slot>{{ title }}</slot>
   </button>
   <!-- </div> -->
@@ -102,7 +110,7 @@ export default {
       type: Boolean,
       default: false,
     },
-    loader:{
+    loader: {
       type: Boolean,
       default: false,
     },
@@ -114,7 +122,7 @@ export default {
       type: String,
       default: "20",
     },
-    icon:{
+    icon: {
       type: String,
       default: null,
     },
@@ -195,11 +203,33 @@ export default {
           this.color === "selected",
       }
     },
+    prefixSpace () {
+      if ( this.prefix && this.icon && !this.loader && this.$slots.default || this.title ) {
+        return 'dd-ml-1.5'
+
+      }
+    },
+    sufixSpace () {
+      if ( this.suffix && this.icon && !this.loader && this.$slots.default || this.title ) {
+        return 'dd-mr-1.5'
+
+      }
+    },
+    btnIconSize () {
+      if ( this.size == 'xs' ) {
+        return '10'
+      } else if ( this.size == 'sm' ) {
+        return '12'
+      } else {
+        return '16'
+      }
+    }
+
   },
   data () {
     return {}
   },
-  components:{
+  components: {
     svgIcon
   }
 }
