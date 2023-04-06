48b22d49efe0660d452146a59b53f071a287dcb3
kamilRiaz <kamil@repairdesk.co>
2023-04-06 14:05:56 +0500

    height wrapper

---
+++ src/components/scrollerApp/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="dd-wraper" :style="styles" >
+<slot></slot>
+  </div>
+</template>
+
+<script setup>
+import { defineProps,computed, onBeforeMount, onMounted, ref, watch } from "vue"
+const props = defineProps( {
+  wrapperProperty:{
+  type: String,
+  default: 'height'
+  },
+  wrapperValue:{
+  type: String,
+  default: '200px'
+  },
+  
+} )  
+const styles = computed(() =>{
+  const styleObj = {}
+    styleObj[props.wrapperProperty] = `calc(100vh - ${props.wrapperValue})`
+    return styleObj
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
