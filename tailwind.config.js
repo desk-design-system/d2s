a5626ddf3f1ec11a72348e483d4119f0599331d8
haseeb-repairdesk <haseeb.ghaffar@repairdesk.co>
2023-05-08 18:24:09 +0500

    accordion and hamburger-icon implmented
    accordion designed
    
    accordoin style implmented
    
    design-done
    
    styling-imporoved

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
   // preflight: false
   },
   theme: {
+    screens: {
+      xs: "321px",
+      sm: "426px",
+      md: "769px",
+      lg: "1024px",
+      xl: "1440px"
+  },
+
     extend: {
       colors: {
        primary: '#d12026',
