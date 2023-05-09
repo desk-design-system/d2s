83ca5430b31fc2b7d6453eac6617b0d34326be82
kamilRiaz <hayyat552@gmail.com>
2023-05-09 15:46:05 +0500

    rules commented

--- src/components/index.js
+++ src/components/index.js
@@ -2,8 +2,8 @@
 import * as DDSystem from "./components.js"
 export * as DDSystem from "./components.js"
 import {useNotification} from "./notification"
-import AllRules from "@vee-validate/rules"
-import {defineRule, configure} from "vee-validate"
+// import AllRules from "@vee-validate/rules"
+// import {defineRule, configure} from "vee-validate"
 const install = (app, options) =>{
   app.provide('$notification', useNotification())
   app.config.globalProperties.$notification = useNotification()
@@ -11,25 +11,25 @@ const install = (app, options) =>{
     app.component(name, DDSystem[name]);
   });
   
-  Object.keys(AllRules).forEach((rule) => {
-    defineRule(rule, AllRules[rule])
-  })
-  configure({
-    generateMessage: localize("en", {
-      messages: {
-        required: "The {field} field is required",
-        alpha: "Only alphabetic values allowed",
-        alpha_num: "Only alphabetic and numeric values allowed",
-        alpha_spaces: "Only alphabetic values allowed",
-        confirmed: "This {field} confirmation does not match",
-        email: "This {field} must be a valid email",
-        integer: "This {field} must be an integer",
-        max: "This {field} not be greater than 0:{max} values",
-        min: "This {field} must be at least 0:{min} values",
-        numeric: "This {field} may only contain numeric values"
-      }
-    })
-  })
+  // Object.keys(AllRules).forEach((rule) => {
+  //   defineRule(rule, AllRules[rule])
+  // })
+  // configure({
+  //   generateMessage: localize("en", {
+  //     messages: {
+  //       required: "The {field} field is required",
+  //       alpha: "Only alphabetic values allowed",
+  //       alpha_num: "Only alphabetic and numeric values allowed",
+  //       alpha_spaces: "Only alphabetic values allowed",
+  //       confirmed: "This {field} confirmation does not match",
+  //       email: "This {field} must be a valid email",
+  //       integer: "This {field} must be an integer",
+  //       max: "This {field} not be greater than 0:{max} values",
+  //       min: "This {field} must be at least 0:{min} values",
+  //       numeric: "This {field} may only contain numeric values"
+  //     }
+  //   })
+  // })
 }
 
 
