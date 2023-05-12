fb3d7d200ea1291ad1b04f5092358a736a68ebd0
haseeb-repairdesk <haseeb.ghaffar@repairdesk.co>
2023-05-12 15:33:05 +0500

    group-button
    removed tested code

--- src/App.vue
+++ src/App.vue
@@ -33,22 +33,12 @@
           v-model="coner"
           :options="options"
         />
-        <div class="dd-mb-10">
-          <DdAccordion
-            rules="required"
-            :srcLink="srcLink"
-            :showAvatar="showAvatar"
-          >
-            Customers
-            <template #content>
-              <div>test dropdown</div>
-            </template>
-          </DdAccordion>
-          <DdPagination
-            :count="count"
-            :limit="limit"
-            :offset="offset"
-            @fetch-data="getData"
+        <div class="dd-mt-10">
+          <dd-group-btn
+            :buttons="buttons"
+            :showIcon="showIcon"
+            :selected="selected"
+            @selected="onButtonSelected"
           />
         </div>
         <tabel />
@@ -60,8 +50,7 @@
 
 <script setup>
 import { ref } from "vue";
-import DdPagination from "./components/Pagination/index.vue";
-import DdAccordion from "./components/Accordion/index.vue";
+import DdGroupBtn from "./components/groupButton/index.vue";
 import DdTopbar from "./components/topbar/index.vue";
 import ddObserver from "./components/validations/ddForm.vue";
 import ddAlert from "./components/alerts/index.vue";
@@ -69,9 +58,7 @@ import ddAvatar from "./components/avatars/index.vue";
 import ddBadge from "./components/badges/index.vue";
 import ddBred from "./components/breadcrumbs/index.vue";
 import ddButton from "./components/buttons/index.vue";
-import ddCard from "./components/card/index.vue";
 import ddCheckBox from "./components/checkbox/index.vue";
-import ddDialog from "./components/dialog/index.vue";
 import ddDivider from "./components/divider/index.vue";
 import ddDrawer from "./components/drawer/index.vue";
 import ddDropDown from "./components/dropdown/index.vue";
@@ -80,6 +67,7 @@ import ddWraper from "./components/scrollerApp/index.vue";
 import ddInput from "./components/input/index.vue";
 import ddMulti from "./components/multiSelect/index.vue";
 import ddSelect from "./components/select/index.vue";
+import { ChevronDoubleDownIcon, ChevronUpIcon } from "@heroicons/vue/solid";
 
 const options = ref([
   {
@@ -156,16 +144,22 @@ const fun = async () => {
 const model = ref(false);
 const text = ref([]);
 const coner = ref("");
-const open = ref(false);
 const srcLink = ref(
   "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
 );
-const showAvatar = ref(false);
-const count = ref(0);
-const limit = ref(5);
-const offset = ref(0);
 
-const getData = () => {
-  count.value = 100;
+const showIcon = ref(true);
+const buttons = [
+  { id: 1, label: "Button 1", color: "white", icon: "ChevronLeft" },
+  { id: 2, label: "Button 2", color: "primary", icon: "ChevronRight" },
+  { id: 3, label: "Button 3", color: "danger", icon: "ChevronDown" },
+  { id: 4, label: "Button 4", color: "warning", icon: "ChevronLeft" },
+  { id: 5, label: "Button 5", color: "selected", icon: "ChevronRight" },
+  { id: 6, label: "", size:"xs", color: "white", icon: "ChevronUp" },
+  { id: 7, label: "", size:"xl", color: "white", icon: "ChevronDown" },
+];
+
+const onButtonSelected = (button) => {
+  console.log(button, "from parent");
 };
 </script>
