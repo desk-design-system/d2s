5178da110df71de4196bb424350e14f5c3425ad4
haseeb-repairdesk <haseeb.ghaffar@repairdesk.co>
2023-05-26 17:08:34 +0500

    confilcts

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,19 @@
         <!-- <ddInput  rules="required|min:6" label="Input" v-model="text"  />
     <ddInput  rules="required|confirmed:@Jazib" label="Confirm" v-model="coner"  /> -->
         <ddMulti label="elo" rules="required" showTooltip customIcon v-model="text" :options="options" />
+        <div class="dd-mb-10">
+          <DdAccordion rules="required" :srcLink="srcLink" :showAvatar="showAvatar">
+            Customers
+            <template #content>
+              <div>test dropdown</div>
+            </template>
+          </DdAccordion>
+          <!-- <DdPagination :count="count" :limit="limit" :offset="offset" @fetch-data="getData" /> -->
+        </div>
+        <ddSelect v-model="selected" :options="filteredOptions" :filterable="filterable" @searchQuery="search"
+          @addQuery="addquery" />
+        <dd-table :rows="rows" :columns="columns" />
+        <tabel />
       </ddObserver>
       <DdSelect v-model="selected" :options="filteredOptions" filterable @searchQuery="search" @addQuery="addquery"
         label="select" />
@@ -28,9 +41,10 @@
 <script setup>
 import { ref, computed } from "vue";
 import DdSelect from "./components/select/index.vue"
+import DdAccordion from "./components/Accordion/index.vue";
+import DdTopbar from "./components/topbar/index.vue";
 import ddObserver from "./components/validations/ddForm.vue";
 import ddAlert from "./components/alerts/index.vue";
-import ddAvatar from "./components/avatars/index.vue";
 import ddBadge from "./components/badges/index.vue";
 import ddBred from "./components/breadcrumbs/index.vue";
 import ddButton from "./components/buttons/index.vue";
@@ -113,17 +127,8 @@ const fun = async () => {
 };
 const model = ref(false);
 const text = ref([]);
-
-
-const query = ref("");
-const filteredOptions = computed(() =>
-  query.value == ""
-    ? people.value
-    : people.value.filter((item) => {
-      return item.name.toLowerCase().includes(query.value.toLowerCase());
-    })
-);
-
+const coner = ref("");
+const open = ref(false);
 const people = ref([
   { value: 1, name: "Leslie Alexander" },
   { value: 2, name: "Aeslie Blexander" },
@@ -134,8 +139,116 @@ const people = ref([
   { value: 7, name: "Jeslie KJlexander" },
   // More people...
 ]);
+const columns = ref([
+  {
+    title: 'Id',
+    value: 'id'
+  },
+  {
+    title: 'User Name',
+    value: 'username'
+  },
+  {
+    title: 'First Name',
+    value: 'firstname'
+  },
+  {
+    title: 'Last Name',
+    value: 'lastname'
+  },
+  {
+    title: 'Email',
+    value: 'email'
+  },
+])
+const rows = ref([
+  {
+    id: 1,
+    username: "Herry007",
+    firstname: "Herry",
+    lastname: "Brook",
+    email: "herry@repairdesk.co"
+  },
+  {
+    id: 2,
+    username: "David2",
+    firstname: "David",
+    lastname: "Jeman",
+    email: "David@repairdesk.co"
+  },
+  {
+    id: 3,
+    username: "Henry0",
+    firstname: "Henry",
+    lastname: "Cavil",
+    email: "henry@repairdesk.co"
+  },
+  {
+    id: 4,
+    username: "Herry007",
+    firstname: "Herry",
+    lastname: "Brook",
+    email: "herry@repairdesk.co"
+  },
+  {
+    id: 5,
+    username: "JSmith",
+    firstname: "John",
+    lastname: "Smith",
+    email: "john.smith@example.com"
+  },
+  {
+    id: 6,
+    username: "LGreen",
+    firstname: "Lucy",
+    lastname: "Green",
+    email: "lucy.green@example.com"
+  },
+  {
+    id: 13,
+    username: "AMiller",
+    firstname: "Alice",
+    lastname: "Miller",
+    email: "alice.miller@example.com"
+  },
+  {
+    id: 14,
+    username: "BDavis",
+    firstname: "Bob",
+    lastname: "Davis",
+    email: "bob.davis@example.com"
+  },
+  {
+    id: 15,
+    username: "KJohnson",
+    firstname: "Kate",
+    lastname: "Johnson",
+    email: "kate.johnson@example.com"
+  }
+])
+
+const srcLink = ref(
+  "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+);
+const showAvatar = ref(false);
+const count = ref(0);
+const limit = ref(5);
+const offset = ref(0);
 const selected = ref("");
 const filterable = ref(false);
+
+const getData = () => {
+  count.value = 100;
+};
+
+const filteredOptions = computed(() =>
+  query.value == ""
+    ? people.value
+    : people.value.filter((item) => {
+      return item.name.toLowerCase().includes(query.value.toLowerCase());
+    })
+);
+
 const search = (data) => {
   query.value = data
 }
--- src/components/table/index.vue
+++ src/components/table/index.vue
@@ -2,38 +2,45 @@
   <div class="dd-mt-8 dd-flow-root">
     <div class="-dd-my-2 -dd-mx-4 dd-overflow-x-auto sm:-dd-mx-6 lg:-dd-mx-8">
       <div class="dd-inline-block dd-min-w-full dd-py-2 dd-align-middle sm:dd-px-6 lg:px-8">
-        <div
-          class="dd-overflow-hidden"
-        >
+        <div class="dd-overflow-hidden">
           <table class="dd-min-w-full dd-divide-y dd-divide-gray-300">
             <thead class="dd-bg-gray-50">
               <tr>
                 <th
-                v-for="col in columns" :key="col.value"
-                  scope="col"
-                  class="dd-py-3.5 dd-pl-4 dd-pr-3 dd-text-left dd-text-xs dd-font-medium dd-text-gray-700 sm:dd-pl-6"
-                >
+                  class="dd-py-3.5 dd-pl-4 dd-text-left dd-text-xs dd-font-medium dd-text-gray-700 sm:dd-pl-4 dd-flex dd-items-center dd-gap-2">
+                  <dd-checkbox v-model="allSelected" @click="selectAllFields" />
+                  <span v-if="selectedId.length > 0">
+                    {{ selectedId.length }} Selected
+                  </span>
+                </th>
+                <th v-for="col in columns" :key="col.value" scope="col"
+                  class="dd-py-3.5 dd-pl-4 dd-pr-3 dd-text-left dd-text-xs dd-font-medium dd-text-gray-700 sm:dd-pl-6">
                   {{ col.title }}
                 </th>
               </tr>
             </thead>
             <tbody class="dd-divide-y dd-divide-gray-200 dd-bg-white">
               <template v-if="rows">
-
-                <tr v-for="(row, index) in rows" :key="index">
-                  <td
-                  v-for="col in columns" :key="col.value"
-                    class="dd-whitespace-nowrap dd-py-4 dd-pl-4 dd-pr-3 dd-text-sm  dd-text-gray-500 sm:dd-pl-6"
-                  >
-                  <slot name="row" :column="col" :row="row" :value="row[col.value]">
-                    {{ row[col.value] }}
-                  </slot>
+                <tr v-for="(row, index) in rows" :key="index" class="hover:dd-bg-gray-100"
+                  :class="[selectedId.includes(row.id) ? 'dd-bg-gray-100' : '']">
+                  <th class="dd-py-3.5 dd-pl-4 dd-text-left dd-text-xs dd-font-medium dd-text-gray-700 sm:dd-pl-4">
+                    <dd-checkbox :checked="selectedId && selectedId.includes(row.id)" :value="row.id"
+                      @click="setChecked(row.id)" />
+                  </th>
+                  <td v-for="col in columns" :key="col.value"
+                    class="dd-whitespace-nowrap dd-py-4 dd-pl-4 dd-pr-3 dd-text-sm  dd-text-gray-500 sm:dd-pl-6">
+                    <slot name="row" :column="col" :row="row" :value="row[col.value]">
+                      {{ row[col.value] }}
+                    </slot>
                   </td>
                 </tr>
               </template>
-              <slot v-else name="noData"  />
+              <slot v-else name="noData" />
             </tbody>
           </table>
+          <div v-if="footer">
+            <DdPagination :count="count" :limit="limit" :offset="offset" @fetch-data="getData" />
+          </div>
         </div>
       </div>
     </div>
@@ -41,10 +48,12 @@
 </template>
 
 <script setup>
-import { ref, computed } from "vue"
-const emits = defineEmits( ['update:modelValue', "change"] )
-const props = defineProps( {
-  
+import { ref } from "vue"
+import DdCheckbox from "../checkbox/index.vue"
+import DdPagination from "../Pagination/index.vue"
+const emits = defineEmits(['update:modelValue', "change"])
+const props = defineProps({
+
   columns: {
     type: Array,
     required: true
@@ -56,13 +65,42 @@ const props = defineProps( {
 
   defaultProps: {
     type: Object,
-    default: () => ( {
+    default: () => ({
       name: 'name',
       value: 'value',
       avatar: 'avatar'
-    } )
+    })
   }
-} )
+})
+
+
+const allSelected = ref(false);
+const selectedId = ref([]);
+const footer = ref(true);
+const limit = ref(5);
+const count = ref(100)
+
+const selectAllFields = () => {
+  if (allSelected.value === false) {
+    selectedId.value = [];
+    props.rows.forEach(row => {
+      selectedId.value.push(row.id);
+    });
+  } else {
+    selectedId.value = [];
+  }
+  allSelected.value = !allSelected.value;
+};
+
+const setChecked = (id) => {
+  const index = selectedId.value.indexOf(id)
+  if (index === -1) {
+    selectedId.value.push(id)
+  } else {
+    selectedId.value.splice(index, 1)
+  }
+  selectedId.value.length > 1 ? allSelected.value = true : allSelected.value = false
+}
 </script>
 
 <style lang="scss" scoped></style>
