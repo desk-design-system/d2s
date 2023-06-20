1a67460d777244b24f6b2d1218035b9f00193dc8
haseeb-repairdesk <132365888+haseeb-repairdesk@users.noreply.github.com>
2023-06-20 00:48:45 -0700

    border fixes

--- src/components/table/index.vue
+++ src/components/table/index.vue
@@ -44,7 +44,7 @@
             size="20" @click="openSearch" />
 
           <div class="dd-w-full dd-cursor-pointer" v-if="search && searchIcon">
-            <div class="dd-flex dd-items-center dd-gap-3 !dd-border-b dd-border-gray-300">
+            <div class="dd-flex dd-items-center dd-gap-3 !dd-border-b dd-border-gray-200">
               <svgIcon v-if="selectedId.length > 0" icon="Search" size="20" class="dd-text-gray-400" />
               <dd-input type="text" v-model="queryInput" @change="searchQuery" class="focus-visible:!dd-border-none dd-w-full"
                 :icon="selectedId.length === 0 ? 'Search' : ''" Border="none" placeholder="Search Ticket"
@@ -65,7 +65,7 @@
         <slot name="actionHeaderSlot" />
         <table class="dd-w-full" :class="
           selectedId.length > 0 && actionHeader == true
-            ? '!dd-border-t dd-border-gray-300'
+            ? '!dd-border-t dd-border-gray-200'
             : '!dd-border-0'
         ">
           <!-- tabel head  -->
@@ -141,7 +141,7 @@
           <tbody class="dd-bg-white" v-if="rows.length > 0" ref="parentRef">
             <template v-if="!defaultRow">
               <tr v-for="(row, index) in rows" :key="index"
-                class="[&>*:nth-child(1)]:!dd-font-normal [&>*:nth-child(2)]:!dd-font-medium dd-relative dd-border-b dd-border-gray-300" :class="[
+                class="[&>*:nth-child(1)]:!dd-font-normal [&>*:nth-child(2)]:!dd-font-medium dd-relative dd-border-b dd-border-gray-100" :class="[
                   selectedId.includes(row.id)
                     ? '[&>*:nth-child(1)]:dd-bg-gray-50 [&>*:nth-child(2)]:dd-bg-gray-50  [&>*:last-child]:!dd-bg-gray-50 dd-bg-gray-50'
                     : '',
@@ -707,7 +707,7 @@ const getHeaderDropdownVal = (data) => {
   bottom: 0;
   left: 0;
   right: 0;
-  border-bottom: 1px solid #d1d5db !important;
+  border-bottom: 1px solid #E5E7EB !important;
 }
 
 .selected-label {
