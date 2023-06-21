c72d352f7a758bc56c28d46cd4a6457ac51f089c
junaidjahan <70884217+junaidjahan@users.noreply.github.com>
2023-06-21 16:57:32 +0500

    feat(icon):add new icons

---
+++ src/components/icons/Hammer.vue
@@ -0,0 +1,14 @@
+<template>
+    <svg width="25.4482299px" height="25.4464546px" viewBox="0 0 25.4482299 25.4464546" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <title>Group 13</title>
+    <g id="✨-New-Ticket-Design-D2s-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <g id="Select-Category-Copy-2" transform="translate(-1303.551472, -487.552298)">
+            <g id="Group" transform="translate(1300.000000, 484.000000)">
+                <polygon id="Path" points="0 0 32 0 32 32 0 32"></polygon>
+                <path d="M15.2186667,13.3333333 L5.37466667,23.224 C4.8477743,23.7473776 4.551472,24.4593428 4.551472,25.202 C4.551472,25.9446572 4.8477743,26.6566224 5.37466667,27.18 C6.47211193,28.2715912 8.2452214,28.2715912 9.34266667,27.18 L19.2186667,17.3333333" id="Path" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                <path d="M24.1613333,20.3906667 L27.6093333,16.9426667 C28.1298426,16.4220002 28.1298426,15.5779998 27.6093333,15.0573333 L17.4946667,4.94266667 C16.9740002,4.42215742 16.1299998,4.42215742 15.6093333,4.94266667 L12.1613333,8.39066667 C11.6408241,8.91133314 11.6408241,9.75533353 12.1613333,10.276 L22.276,20.3906667 C22.7966665,20.9111759 23.6406669,20.9111759 24.1613333,20.3906667 Z" id="Path" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+            </g>
+        </g>
+    </g>
+</svg>
+</template>
---
+++ src/components/icons/TV.vue
@@ -0,0 +1,14 @@
+<template>
+    <svg width="26px" height="24.6666667px" viewBox="0 0 26 24.6666667" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <title>Group 8</title>
+        <g id="✨-New-Ticket-Design-D2s-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+            <g id="Select-Category-Copy-2" transform="translate(-1092.000000, -383.000000)">
+                <g id="Group" transform="translate(1089.000000, 380.000000)">
+                    <polygon id="Path" points="0 0 32 0 32 32 0 32"></polygon>
+                    <path d="M4,12 C4,10.5272407 5.19390733,9.33333333 6.66666667,9.33333333 L25.3333333,9.33333333 C26.8060927,9.33333333 28,10.5272407 28,12 L28,24 C28,25.4727593 26.8060927,26.6666667 25.3333333,26.6666667 L6.66666667,26.6666667 C5.19390733,26.6666667 4,25.4727593 4,24 L4,12 Z" id="Path" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                    <polyline id="Path" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" points="21.3333333 4 16 9.33333333 10.6666667 4"></polyline>
+                </g>
+            </g>
+        </g>
+    </svg>
+</template>
