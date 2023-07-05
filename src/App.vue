6284ae4db14e6b28ed8692585a6b21e45cb42371
haseeb-ghaffar <hbghaffar01@gmail.com>
2023-07-05 00:37:56 -0700

    tree base structure

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <div>
-    <dd-table :rows="rows" :columns="columnVal" :buttons="buttons" :Actions="Actions" :values="values" footer checkBoxProp
+    <!-- <dd-table :rows="rows" :columns="columnVal" :buttons="buttons" :Actions="Actions" :values="values" footer checkBoxProp
       fixedHeight rowKey="id" hoveringRow lastCell fixed actionHeader emptyState actionsPanel sortIcon settingbarIcon
-      searchIcon @updateSettings="updateSettings" dragDrop />
+      searchIcon @updateSettings="updateSettings" dragDrop /> -->
+      <dd-tree />
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
 import DdTable from "./components/table/index.vue";
+import DdTree from "./components/Tree/index.vue"
 
 let columns = ref([
   {
---
+++ src/components/Tree/Actions.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="dd-flex dd-items-center dd-justify-center">
+    <DdGroupButton class="dd-p-1">
+      <dd-Button
+      v-for="button in buttons"
+      :key="button?.id"
+      :color="button?.color"
+      :size="button?.size"
+      @click="selectButton(button)"
+    >
+      {{ button?.label }}
+      <svgIcon
+        v-if="showIcon"
+        class="dd-flex dd-items-center dd-justify-center"
+        :icon="button?.icon"
+        :size="button?.size"
+      />
+    </dd-Button>
+    </DdGroupButton>
+  </div>
+</template>
+
+<script setup>
+import { DdButton } from "../components";
+import DdGroupButton from "../groupButton/index.vue";
+import svgIcon from "../svgIcon/index.vue"
+const emit = defineEmits(["selected"]);
+const props = defineProps({
+  showIcon: {
+    type: Boolean,
+    default: false,
+  },
+  buttons: {
+    type: Array,
+    required: true,
+  },
+});
+
+const selectButton = (button) => {
+  emit("selected", button);
+};
+</script>
+
+<style>
+
+</style>
