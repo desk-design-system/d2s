2b998fbda712bcaa67de3255ca220e0d22fb4061
haseeb-ghaffar <hbghaffar01@gmail.com>
2023-07-06 00:15:33 -0700

    tree function completed

--- src/components/Tree/Actions.vue
+++ src/components/Tree/Actions.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="dd-flex dd-items-center dd-justify-center">
-    <DdGroupButton class="dd-p-1">
+    <DdGroupButton>
       <dd-Button
       v-for="button in buttons"
       :key="button?.id"
       :color="button?.color"
       :size="button?.size"
       @click="selectButton(button)"
+      class="!dd-h-6"
     >
       {{ button?.label }}
       <svgIcon
-        v-if="showIcon"
         class="dd-flex dd-items-center dd-justify-center"
-        :icon="button?.icon"
+        :icon="button?.icon ?? ''"
         :size="button?.size"
       />
     </dd-Button>
@@ -21,15 +21,11 @@
 </template>
 
 <script setup>
-import { DdButton } from "../components";
+import DdButton from "../buttons/index.vue";
 import DdGroupButton from "../groupButton/index.vue";
 import svgIcon from "../svgIcon/index.vue"
 const emit = defineEmits(["selected"]);
 const props = defineProps({
-  showIcon: {
-    type: Boolean,
-    default: false,
-  },
   buttons: {
     type: Array,
     required: true,
--- src/components/icons/SquareMinus.vue
+++ src/components/icons/SquareMinus.vue
@@ -6,15 +6,19 @@
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <path
-      d="M8.0013 3.50003V12.8334"
+    <rect
+      x="2.66797"
+      y="2.76663"
+      width="10.6667"
+      height="10.6667"
+      rx="1.33333"
       stroke="#6B7280"
       stroke-width="1.25"
       stroke-linecap="round"
       stroke-linejoin="round"
     />
     <path
-      d="M3.33203 8.16668H12.6654"
+      d="M6 8.09997H10"
       stroke="#6B7280"
       stroke-width="1.25"
       stroke-linecap="round"
