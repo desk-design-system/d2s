8ba0a003e8b40f48f47f0ebfb7adf91477addfc0
haseeb-ghaffar <hbghaffar01@gmail.com>
2023-07-12 04:20:58 -0700

    input fixes

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
 <template>
   <div>
-    <!-- <dd-table :rows="rows" :columns="columnVal" :buttons="buttons" :Actions="Actions" :values="values" footer checkBoxProp
+    <dd-table :rows="rows" :columns="columnVal" :buttons="buttons" :Actions="Actions" :values="values" footer checkBoxProp
       fixedHeight rowKey="id" hoveringRow lastCell fixed actionHeader emptyState actionsPanel sortIcon settingbarIcon
-      searchIcon @updateSettings="updateSettings" dragDrop /> -->
-      <dd-input suffix v-model="modalVal" name="modalVal" type="password" rules="required" hintTextColor="gray" hintText="daksbjkdaskjbjk" />
+      searchIcon @updateSettings="updateSettings" dragDrop />
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
 import DdTable from "./components/table/index.vue";
-import DdInput from "./components/input/index.vue"
-
-const modalVal = ref("");
 
 let columns = ref([
   {
