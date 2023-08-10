f0f5d0b1f3e4b3c8558660287c233cc235eca4a8
junaidjahan <70884217+junaidjahan@users.noreply.github.com>
2023-08-10 15:39:51 +0500

    feat(accordion): props and style changes for #192

--- src/components/Accordion/index.vue
+++ src/components/Accordion/index.vue
@@ -2,59 +2,61 @@
   <div
     v-for="(tab, index) of tabs"
     :key="index + 1"
-    class=" dd-border dd-border-gray-200 dd-rounded-md dd-mb-3 dd-bg-white dd-overflow-hidden"
+    class="dd-border dd-border-gray-200 dd-rounded-md dd-mb-3 dd-overflow-hidden dd-shadow-sm"
+    :class="[isActive && isActive == index+1 ? colorClass : 'dd-bg-white dd-border-gray-200']"
     v-bind="$attrs"
   >
+
     <div class="dd-cursor-pointer" @click="changeActiveIndex(index + 1)">
       <component
         v-if="tab.children && tab.children.header"
         :is="tab.children.header"
       >
       </component>
-      <div v-else class="dd-flex dd-justify-between dd-p-3">
-        <div class="dd-flex">
+      <div v-else class="dd-flex dd-justify-between dd-px-6 dd-py-5">
+        <div class="dd-flex dd-items-center dd-gap-x-2">
             <!-- @slot Use this slot header -->
           <component
-            v-if="tab.children && tab.children.prepend"
-            :is="tab.children.prepend"
+            v-if="tab.children && tab.children.left"
+            :is="tab.children.left"
           >
           </component>
           <dd-svg-icon
             v-else-if="!!getTabProp(tab, 'prepend-icon')"
             :icon="getTabProp(tab, 'prepend-icon')"
-            size="20"
-            class="dd-mr-2"
+            size="28"
+            
           >
           </dd-svg-icon>
 
           <p
             v-if="tab.props && tab.props.title"
-            class="dd-font-medium dd-text-sm dd-text-gray-700 dd-text-left"
+            class="dd-font-medium dd-text-md dd-text-gray-700 dd-text-left"
           >
             {{ tab.props.title ? tab.props.title : `Tab-${index + 1}` }}
           </p>
         </div>
-        <div class="dd-flex">
+        <div class="dd-flex dd-items-center dd-gap-2">
           <component
-            v-if="tab.children && tab.children.append"
-            :is="tab.children.append"
+            v-if="tab.children && tab.children.right"
+            :is="tab.children.right"
           >
           </component>
           <dd-svg-icon
             v-else-if="!!getTabProp(tab, 'append-icon')"
             :icon="getTabProp(tab, 'append-icon')"
-            size="20"
-            class="dd-mr-2"
+            size="28"
+            
           >
           </dd-svg-icon>
-          <div class="dd-flex dd-align-center dd-h-full dd-pt-0.5" >
+          <div class="dd-flex dd-items-center dd-h-full dd-pt-0.5" >
             <dd-svg-icon
               icon="ChevronDown"
               size="16"
               class="dd-text-gray-400 dd-text-center"
               aria-hidden="true"
               :class="[
-                ddActive == index + 1
+                isActive == index + 1
                   ? 'dd-transform dd-rotate-180'
                   : 'dd-transform dd-rotate-0',
               ]"
@@ -86,13 +88,18 @@ multiple: {
   type: Boolean,
   default: false,
 },
+color: {
+  type: String,
+  default: null,
+},
+
 });
 
 const emit = defineEmits(["update:active"]);
 const slots = useSlots();
 
 const ACCORDION_TAB = "AccordionTab";
-let ddActive = ref(props.active);
+let isActive = ref(props.active);
 
 
 //Methods
@@ -115,25 +122,30 @@ const tabs = computed(() => {
   }, []);
 });
 
+const colorClass = computed(() => {
+    return ` dd-bg-gradient-to-l dd-from-${props.color}-100 dd-border-${props.color}-200`
+});
+
+
 const changeActiveIndex = (index) => {
   const active = isTabActive(index);
   if (props.multiple) {
     if (active) {
-      ddActive.value = ddActive.value.filter((i) => i !== index);
+      isActive.value = isActive.value.filter((i) => i !== index);
     } else {
-      if (ddActive.value) ddActive.value.push(index);
-      else ddActive.value = [index];
+      if (isActive.value) isActive.value.push(index);
+      else isActive.value = [index];
     }
   } else {
-    ddActive.value = ddActive.value === index ? null : index;
+    isActive.value = isActive.value === index ? null : index;
   }
   emit("update:active", index);
 };
 
 const isTabActive = (index) => {
 return props.multiple
-  ? ddActive.value && ddActive.value.includes(index)
-  : ddActive.value === index;
+  ? isActive.value && isActive.value.includes(index)
+  : isActive.value === index;
 };
 
 const getTabProp = (tab, name) => {
--- src/components/AccordionTab/index.vue
+++ src/components/AccordionTab/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="p-3">
+    <div class="dd-px-6" >
         <slot>
 
         </slot>
