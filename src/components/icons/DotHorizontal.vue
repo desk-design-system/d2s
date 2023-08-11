6e2486f8092e3d2c6bba94aa1310f69a2626d0ba
haseeb-ghaffar <hbghaffar01@gmail.com>
2023-08-11 05:57:37 -0700

    icon fixes dot

--- src/components/icons/DotHorizontal.vue
+++ src/components/icons/DotHorizontal.vue
@@ -1,7 +1,7 @@
 <template>
   <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-dots" width="16" height="16" viewBox="0 0 24 24" stroke-width="1.5" fill="none" stroke-linecap="round" stroke-linejoin="round">
-    <path stroke="currentColor" d="M5 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
-    <path stroke="currentColor" d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
-    <path stroke="currentColor" d="M19 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+    <path stroke="#6B7280" d="M5 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+    <path stroke="#6B7280" d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+    <path stroke="#6B7280" d="M19 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
   </svg>
 </template>
