2edf84a4005a90fc8b08640dbc54f7b3761e4808
haseeb-ghaffar <hbghaffar01@gmail.com>
2023-08-15 00:21:54 -0700

    conflict fixes

--- src/components/Accordion/index.vue
+++ src/components/Accordion/index.vue
@@ -1,77 +1,39 @@
 <template>
-  <div
-    v-for="(tab, index) of tabs"
-    :key="index + 1"
-    class="dd-border dd-border-gray-200 dd-rounded-md dd-mb-3 dd-overflow-hidden dd-shadow-sm"
-    :class="[
+  <div v-for="(tab, index) of tabs" :key="index + 1"
+    class="dd-border dd-border-gray-200 dd-rounded-md dd-mb-3 dd-overflow-hidden dd-shadow-sm" :class="[
       isActive && isActive == index + 1
         ? colorClass
         : 'dd-bg-white dd-border-gray-200',
-    ]"
-    v-bind="$attrs"
-  >
+    ]" v-bind="$attrs">
     <div class="dd-cursor-pointer" @click="changeActiveIndex(index + 1)">
-      <component
-        v-if="tab.children && tab.children.header"
-        :is="tab.children.header"
-      >
+      <component v-if="tab.children && tab.children.header" :is="tab.children.header">
       </component>
       <div v-else class="dd-flex dd-justify-between dd-px-6 dd-py-5">
         <div class="dd-flex dd-items-center dd-gap-x-2">
           <!-- @slot Use this slot header -->
-          <component
-            v-if="tab.children && tab.children.left"
-            :is="tab.children.left"
-          >
+          <component v-if="tab.children && tab.children.left" :is="tab.children.left">
           </component>
-          <dd-svg-icon
-            v-else-if="!!getTabProp(tab, 'prepend-icon')"
-            :icon="getTabProp(tab, 'prepend-icon')"
-            size="28"
-          >
+          <dd-svg-icon v-else-if="!!getTabProp(tab, 'prepend-icon')" :icon="getTabProp(tab, 'prepend-icon')" size="28">
           </dd-svg-icon>
 
-          <p
-            v-if="tab.props && tab.props.title"
-            class="dd-font-medium dd-text-md dd-text-gray-700 dd-text-left"
-          >
+          <p v-if="tab.props && tab.props.title" class="dd-font-medium dd-text-md dd-text-gray-700 dd-text-left">
             {{ tab.props.title ? tab.props.title : `Tab-${index + 1}` }}
           </p>
         </div>
         <div class="dd-flex dd-items-center dd-gap-2">
-          <dd-badge
-            v-if="tab.props?.badge"
-            :title="tab.props?.badge"
-            :closable="tab.props?.closable"
-            :color="tab.props?.color"
-            :rounded="tab.props?.rounded"
-            :size="tab.props?.size"
-            :dot="tab.props?.dot"
-            @close="$emit('close')"
-          />
-          <component
-            v-if="tab.children && tab.children.right"
-            :is="tab.children.right"
-          >
+          <dd-badge v-if="tab.props?.badge" :title="tab.props?.badge" :closable="tab.props?.closable"
+            :color="tab.props?.color" :rounded="tab.props?.rounded" :size="tab.props?.size" :dot="tab.props?.dot"
+            @close="$emit('close')" />
+          <component v-if="tab.children && tab.children.right" :is="tab.children.right">
           </component>
-          <dd-svg-icon
-            v-else-if="!!getTabProp(tab, 'append-icon')"
-            :icon="getTabProp(tab, 'append-icon')"
-            size="28"
-          >
+          <dd-svg-icon v-else-if="!!getTabProp(tab, 'append-icon')" :icon="getTabProp(tab, 'append-icon')" size="28">
           </dd-svg-icon>
           <div class="dd-flex dd-items-center dd-h-full dd-pt-0.5">
-            <dd-svg-icon
-              icon="ChevronDown"
-              size="16"
-              class="dd-text-gray-400 dd-text-center"
-              aria-hidden="true"
-              :class="[
-                isActive == index + 1
-                  ? 'dd-transform dd-rotate-180'
-                  : 'dd-transform dd-rotate-0',
-              ]"
-            />
+            <dd-svg-icon icon="ChevronDown" size="16" class="dd-text-gray-400 dd-text-center" aria-hidden="true" :class="[
+              isActive == index + 1
+                ? 'dd-transform dd-rotate-180'
+                : 'dd-transform dd-rotate-0',
+            ]" />
           </div>
         </div>
       </div>
