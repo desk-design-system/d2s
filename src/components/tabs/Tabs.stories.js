2635a09a9b4d5f06f1f4801cb9eed65dc9ce3c74
haseeb-ghaffar <hbghaffar01@gmail.com>
2023-08-17 06:06:34 -0700

    tabs component revamped

--- src/components/tabs/Tabs.stories.js
+++ src/components/tabs/Tabs.stories.js
@@ -1,6 +1,6 @@
-import DdTabs from "./index.vue"
-import { action } from "@storybook/addon-actions"
-import {ref} from 'vue'
+import DdTabs from "./index.vue";
+import { action } from "@storybook/addon-actions";
+import { ref } from "vue";
 import {
   ExclamationIcon,
   XCircleIcon,
@@ -9,99 +9,435 @@ import {
 } from "@heroicons/vue/solid";
 // More on default export: https://storybook.js.org/docs/vue/writing-stories/introduction#default-export
 export default {
-  title: 'Molecules/Tabs',
+  title: "Molecules/Tabs",
   component: DdTabs,
-  // More on argTypes: https://storybook.js.org/docs/vue/api/argtypes
+};
+
+export const Underline36 = {
+  render: (args) => ({
+    components: {
+      DdTabs,
+    },
+    setup() {
+      const onClickMethod = () => action("clicked");
+      const selected = ref(1);
+      const value = ref([
+        {
+          name: "Tab 1",
+          value: 1,
+          icon: ExclamationIcon,
+        },
+        {
+          name: "Tab 2",
+          value: 2,
+          icon: XCircleIcon,
+        },
+        {
+          name: "Tab 3",
+          value: 3,
+          icon: CheckCircleIcon,
+        },
+        {
+          name: "Tab 4",
+          value: 4,
+          icon: InformationCircleIcon,
+        },
+      ]);
+      return { value, selected, args, onClickMethod };
+    },
+    template: `<dd-tabs v-model="selected" :tabs="value" v-bind="args" />`,
+  }),
   argTypes: {
-     tabs:{
-      description: "Pass data array in tabs and tabs should containe name and value",
-      table:{
-        defaultValue:{
-          summary: "null"
-        }
-      }
-     },
-    type: {
-      control: { type: 'select' },
-      options: ["default", "pill"],
-      description: "You chnage the style of the tabs by simle chnage gthe prop type value which accepte value default / pill",
-      table:{
-        defaultValue:{
-          summary: "default"
-        }
-      }
-    }, 
-    borderBottom: {
-      description: "In Default ui type if you want to show divider under tabs simple pass prop ",
+    tabs: {
+      description:
+        "Pass data array in tabs and tabs should containe name and value",
       table: {
-          defaultValue: {
-              summary: false
-          }
-      }
-  },
-  icon: {
-    description: "I Default ui type if you want to show icons before text tab simple pass prop ",
-    table: {
         defaultValue: {
-            summary: false
-        }
-    }
-},
+          summary: "null",
+        },
+      },
+    },
+    type: {
+      control: { type: "select" },
+      options: ["underlined36", "underlined44", "pill"],
+      description:
+        "You chnage the style of the tabs by simle chnage gthe prop type value which accepte value default / pill",
+      table: {
+        defaultValue: {
+          summary: "default",
+        },
+      },
+    },
+    borderBottom: {
+      description:
+        "In Default ui type if you want to show divider under tabs simple pass prop and it doesnot work on type='pill' ",
+      table: {
+        defaultValue: {
+          summary: false,
+        },
+      },
+    },
+    rightBadge: {
+      description:
+        "use rightBadge to add badge in tabs. It is using DdBadge component in D2s",
+      table: {
+        defaultValue: {
+          summary: false,
+        },
+      },
+    },
+    change: {
+      description:
+        "return the selected tabs object and it also return the selected tabs object when page is mounted",
+      table: {
+        defaultValue: {
+          summary: Event,
+        },
+      },
+    },
   },
 };
 
-// More on component templates: https://storybook.js.org/docs/vue/writing-stories/introduction#using-args
-const Template = (args) => ({
-  // Components used in your story `template` are defined in the `components` object
-  components: { DdTabs },
-  // The story's `args` need to be mapped into the template through the `setup()` method
-  setup() {
-    const onClickMethod =  (() => action("clicked"))
-    const selected = ref(1)
-    const value = ref([
-    {
-      name: 'Tab 1',
-      value: 1,
-      icon: ExclamationIcon
+export const Underline44 = {
+  render: (args) => ({
+    components: {
+      DdTabs,
     },
-    {
-      name: 'Tab 2',
-      value: 2,
-      icon: XCircleIcon
+    setup() {
+      const onClickMethod = () => action("clicked");
+      const selected = ref(1);
+      const value = ref([
+        {
+          name: "Tab 1",
+          value: 1,
+          icon: ExclamationIcon,
+        },
+        {
+          name: "Tab 2",
+          value: 2,
+          icon: XCircleIcon,
+        },
+        {
+          name: "Tab 3",
+          value: 3,
+          icon: CheckCircleIcon,
+        },
+        {
+          name: "Tab 4",
+          value: 4,
+          icon: InformationCircleIcon,
+        },
+      ]);
+      return { value, selected, args, onClickMethod };
     },
-    {
-      name: 'Tab 3',
-      value: 3,
-      icon: CheckCircleIcon
+    template: `<dd-tabs v-model="selected" :tabs="value" type="underlined44" v-bind="args" />`,
+  }),
+  argTypes: {
+    tabs: {
+      description:
+        "Pass data array in tabs and tabs should containe name and value",
+      table: {
+        defaultValue: {
+          summary: "null",
+        },
+      },
     },
-    {
-      name: 'Tab 4',
-      value: 4,
-      icon: InformationCircleIcon
+    type: {
+      control: { type: "select" },
+      options: ["underlined36", "underlined44", "pill"],
+      description:
+        "You chnage the style of the tabs by simle chnage gthe prop type value which accepte value default / pill",
+      table: {
+        defaultValue: {
+          summary: "default",
+        },
+      },
     },
-  ])
-    return { value, selected, args, onClickMethod };
-
+    borderBottom: {
+      description:
+        "In Default ui type if you want to show divider under tabs simple pass prop ",
+      table: {
+        defaultValue: {
+          summary: false,
+        },
+      },
+    },
+    rightBadge: {
+      description:
+        "use rightBadge to add badge in tabs. It is using DdBadge component in D2s",
+      table: {
+        defaultValue: {
+          summary: false,
+        },
+      },
+    },
   },
-  // And then the `args` are bound to your component with `v-bind="args"`
-  template: '<dd-tabs  v-bind="args" v-model="selected"  :tabs="value"  />  ',
-});
-
-
-
-export const Default = Template.bind({});
-export const Pill = Template.bind({});
-export const WithBorder = Template.bind({});
-export const Icons = Template.bind({});
-// More on args: https://storybook.js.org/docs/vue/writing-stories/args
-Pill.args = {
-  type: 'pill',
-};
-WithBorder.args = {
-  borderBottom: true
-};
-Icons.args = {
-  icon: true,
 };
 
+export const Pill = {
+  render: (args) => ({
+    components: {
+      DdTabs,
+    },
+    setup() {
+      const onClickMethod = () => action("clicked");
+      const selected = ref(1);
+      const value = ref([
+        {
+          name: "Tab 1",
+          value: 1,
+          icon: ExclamationIcon,
+        },
+        {
+          name: "Tab 2",
+          value: 2,
+          icon: XCircleIcon,
+        },
+        {
+          name: "Tab 3",
+          value: 3,
+          icon: CheckCircleIcon,
+        },
+        {
+          name: "Tab 4",
+          value: 4,
+          icon: InformationCircleIcon,
+        },
+      ]);
+      return { value, selected, args, onClickMethod };
+    },
+    template: `<dd-tabs v-model="selected" :tabs="value" type="Pill" v-bind="args" />`,
+  }),
+  argTypes: {
+    tabs: {
+      description:
+        "Pass data array in tabs and tabs should containe name and value",
+      table: {
+        defaultValue: {
+          summary: "null",
+        },
+      },
+    },
+    type: {
+      control: { type: "select" },
+      options: ["underlined36", "underlined44", "pill"],
+      description:
+        "You chnage the style of the tabs by simle chnage gthe prop type value which accepte value default / pill",
+      table: {
+        defaultValue: {
+          summary: "default",
+        },
+      },
+    },
+    borderBottom: {
+      description:
+        "In Default ui type if you want to show divider under tabs simple pass prop ",
+      table: {
+        defaultValue: {
+          summary: false,
+        },
+      },
+    },
+    rightBadge: {
+      description:
+        "use rightBadge to add badge in tabs. It is using DdBadge component in D2s",
+      table: {
+        defaultValue: {
+          summary: false,
+        },
+      },
+    },
+  },
+};
 
+export const UnderlinedFeatured = {
+  render: (args) => ({
+    components: {
+      DdTabs,
+    },
+    setup() {
+      const onClickMethod = () => action("clicked");
+      const selected = ref(1);
+      const value = ref([
+        {
+          name: "Tab 1",
+          value: 1,
+          icon: ExclamationIcon,
+        },
+        {
+          name: "Tab 2",
+          value: 2,
+          icon: XCircleIcon,
+        },
+        {
+          name: "Tab 3",
+          value: 3,
+          icon: CheckCircleIcon,
+        },
+        {
+          name: "Tab 4",
+          value: 4,
+          icon: InformationCircleIcon,
+        },
+      ]);
+      return { value, selected, args, onClickMethod };
+    },
+    template: `<dd-tabs v-model="selected" :tabs="value" type="underlined44" rightBadge borderBottom v-bind="args" />`,
+  }),
+  argTypes: {
+    tabs: {
+      description:
+        "Pass data array in tabs and tabs should containe name and value",
+      table: {
+        defaultValue: {
+          summary: "null",
+        },
+      },
+    },
+    type: {
+      control: { type: "select" },
+      options: ["underlined36", "underlined44", "pill"],
+      description:
+        "You chnage the style of the tabs by simle chnage gthe prop type value which accepte value default / pill",
+      table: {
+        defaultValue: {
+          summary: "default",
+        },
+      },
+    },
+    borderBottom: {
+      description:
+        "In Default ui type if you want to show divider under tabs simple pass prop ",
+      table: {
+        defaultValue: {
+          summary: false,
+        },
+      },
+    },
+    rightBadge: {
+      description:
+        "use rightBadge to add badge in tabs. It is using DdBadge component in D2s",
+      table: {
+        defaultValue: {
+          summary: false,
+        },
+      },
+    },
+  },
+};
+
+export const PillFeatured = {
+  render: (args) => ({
+    components: {
+      DdTabs,
+    },
+    setup() {
+      const onClickMethod = () => action("clicked");
+      const selected = ref(1);
+      const value = ref([
+        {
+          name: "Tab 1",
+          value: 1,
+          icon: ExclamationIcon,
+        },
+        {
+          name: "Tab 2",
+          value: 2,
+          icon: XCircleIcon,
+        },
+        {
+          name: "Tab 3",
+          value: 3,
+          icon: CheckCircleIcon,
+        },
+        {
+          name: "Tab 4",
+          value: 4,
+          icon: InformationCircleIcon,
+        },
+      ]);
+      return { value, selected, args, onClickMethod };
+    },
+    template: `<dd-tabs v-model="selected" :tabs="value" type="pill" rightBadge v-bind="args" />`,
+  }),
+  argTypes: {
+    tabs: {
+      description:
+        "Pass data array in tabs and tabs should containe name and value",
+      table: {
+        defaultValue: {
+          summary: "null",
+        },
+      },
+    },
+    type: {
+      control: { type: "select" },
+      options: ["underlined36", "underlined44", "pill"],
+      description:
+        "You chnage the style of the tabs by simle chnage gthe prop type value which accepte value default / pill",
+      table: {
+        defaultValue: {
+          summary: "default",
+        },
+      },
+    },
+    borderBottom: {
+      description:
+        "In Default ui type if you want to show divider under tabs simple pass prop ",
+      table: {
+        defaultValue: {
+          summary: false,
+        },
+      },
+    },
+    rightBadge: {
+      description:
+        "use rightBadge to add badge in tabs. It is using DdBadge component in D2s",
+      table: {
+        defaultValue: {
+          summary: false,
+        },
+      },
+    },
+  },
+};
+
+Underline36.parameters = {
+  docs: {
+    source: {
+      code: `<dd-tabs v-model="selected" :tabs="value" />`,
+    },
+  },
+};
+
+Underline44.parameters = {
+  docs: {
+    source: {
+      code: `<dd-tabs v-model="selected" :tabs="value" type="underlined44" />`,
+    },
+  },
+};
+
+Pill.parameters = {
+  docs: {
+    source: {
+      code: `<dd-tabs v-model="selected" :tabs="value" type="pill" />`,
+    },
+  },
+};
+
+UnderlinedFeatured.parameters = {
+  docs: {
+    source: {
+      code: `<dd-tabs v-model="selected" :tabs="value" type="underlined44" rightBadge borderBottom />`,
+    },
+  },
+};
+
+PillFeatured.parameters = {
+  docs: {
+    source: {
+      code: `<dd-tabs v-model="selected" :tabs="value" type="pill" rightBadge />`,
+    },
+  },
+};
