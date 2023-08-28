373a574d6f59fbeaa649f209fcc4ae9bebdae8a1
kamilRiaz <hayyat552@gmail.com>
2023-08-28 17:09:17 +0500

    new icons added

---
+++ src/components/icons/Currency-Dollar.vue
@@ -0,0 +1,13 @@
+<template>
+<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_68_4828)">
+<path d="M11.1333 5.33333C11.0006 4.95683 10.7583 4.62864 10.4375 4.39102C10.1167 4.1534 9.73217 4.01722 9.33334 4H6.66667C6.13624 4 5.62753 4.21071 5.25246 4.58579C4.87739 4.96086 4.66667 5.46957 4.66667 6C4.66667 6.53043 4.87739 7.03914 5.25246 7.41421C5.62753 7.78929 6.13624 8 6.66667 8H9.33334C9.86377 8 10.3725 8.21071 10.7476 8.58579C11.1226 8.96086 11.3333 9.46957 11.3333 10C11.3333 10.5304 11.1226 11.0391 10.7476 11.4142C10.3725 11.7893 9.86377 12 9.33334 12H6.66667C6.26784 11.9828 5.88328 11.8466 5.56249 11.609C5.24171 11.3714 4.99937 11.0432 4.86667 10.6667" stroke="currentColor" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M8 2V4M8 12V14" stroke="currentColor" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
+</g>
+<defs>
+<clipPath id="clip0_68_4828">
+<rect width="16" height="16" fill="white"/>
+</clipPath>
+</defs>
+</svg>
+</template>
---
+++ src/components/icons/Percentage.vue
@@ -0,0 +1,14 @@
+<template>
+<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_1048_15189)">
+<path d="M10.6667 11.3333C10.6667 11.5101 10.7369 11.6797 10.8619 11.8047C10.987 11.9298 11.1565 12 11.3334 12C11.5102 12 11.6797 11.9298 11.8048 11.8047C11.9298 11.6797 12 11.5101 12 11.3333C12 11.1565 11.9298 10.987 11.8048 10.8619C11.6797 10.7369 11.5102 10.6667 11.3334 10.6667C11.1565 10.6667 10.987 10.7369 10.8619 10.8619C10.7369 10.987 10.6667 11.1565 10.6667 11.3333Z" stroke="currentColor" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M4 4.66667C4 4.84348 4.07024 5.01305 4.19526 5.13807C4.32029 5.2631 4.48986 5.33333 4.66667 5.33333C4.84348 5.33333 5.01305 5.2631 5.13807 5.13807C5.2631 5.01305 5.33333 4.84348 5.33333 4.66667C5.33333 4.48986 5.2631 4.32029 5.13807 4.19526C5.01305 4.07024 4.84348 4 4.66667 4C4.48986 4 4.32029 4.07024 4.19526 4.19526C4.07024 4.32029 4 4.48986 4 4.66667Z" stroke="currentColor" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M4 12L12 4" stroke="currentColor" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
+</g>
+<defs>
+<clipPath id="clip0_1048_15189">
+<rect width="16" height="16" fill="white"/>
+</clipPath>
+</defs>
+</svg>
+</template>
